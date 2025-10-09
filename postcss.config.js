b590e518795f31a83dd98eba610fc236bbe2fc8d
Niklas Lehnskov Miranda <nlm@djursconsult.dk>
2025-10-09 10:59:21 +0200

    PostCSS adjusted.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+const prefixer = require('postcss-prefix-selector');
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    prefixer({
+      prefix: '#esg-root',
+      transform(prefix, selector, prefixedSelector) {
+        // Undgå at prefixe html og body
+        if (selector.startsWith('html') || selector.startsWith('body')) {
+          return selector;
+        }
+        return prefixedSelector;
+      },
+    }),
   ],
 };
