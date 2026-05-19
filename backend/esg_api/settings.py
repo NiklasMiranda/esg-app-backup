cf20fe494db1391eb15e6c02530d62611b7e461d
EsbenSB <esb57676@gmail.com>
2026-05-19 13:21:02 +0200

    feat: add Docker-based development environment

--- backend/esg_api/settings.py
+++ backend/esg_api/settings.py
@@ -112,11 +112,11 @@ WSGI_APPLICATION = 'esg_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'esg_db',
-        'USER': 'postgres',
-        'PASSWORD': '#Djurs123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
