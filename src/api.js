f77ab5aa4e71ca5e9afda878151a1dc63683dbf7
EsbenSB <esb57676@gmail.com>
2026-05-21 15:17:02 +0200

    extract esg calculations api module

--- src/api.js
+++ src/api.js
@@ -18,30 +18,7 @@ export {
   fetchIaQuestionsFromApi,
 } from 'features/esg-calculator/api/questionsApi';
 
-/**
- * Fetches calculation results from the Django DRF API.
- * @param {number} companyId The ID of the company to fetch results for.
- * @param {number} year The year for which to fetch results.
- * @returns {Promise<Object>} A promise that resolves to the calculated ESG results.
- */
-export const fetchCalculationResultsFromApi = async (companyId, year) => {
-  try {
-    const response = await fetch(
-      `${DJANGO_API_BASE_URL}calculation-results/${companyId}/${year}/`,
-      {
-        headers: createAuthHeader(),
-      },
-    );
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching calculation results:', error);
-    throw error;
-  }
-};
+export { fetchCalculationResultsFromApi } from './features/esg-calculator/api/calculationsApi';
 
 /**
  * Fetches a list of years for which a company has saved data.
